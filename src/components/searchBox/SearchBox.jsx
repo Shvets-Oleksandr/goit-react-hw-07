7073719872d5e8a585f557ed784d9a1f9e506da0
Oleksandr Shvets <shvec.olx@gmail.com>
2024-11-04 20:13:38 +0100

    add filtersSlise

--- src/components/searchBox/SearchBox.jsx
+++ src/components/searchBox/SearchBox.jsx
@@ -1,11 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux';
 
 import css from './SearchBox.module.css';
-import { setFilterContact } from '../../redux/contactsSlice';
+import { setFilterContact } from '../../redux/filtersSlice';
 
 const SearchBox = () => {
   const dispatch = useDispatch();
-  const filter = useSelector(state => state.contacts.filters.name);
+  const filter = useSelector(state => state.filters.name);
 
   const handleFilterChange = value => {
     dispatch(setFilterContact(value));
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,67 +1,57 @@
 import { createSlice, createSelector } from '@reduxjs/toolkit';
 import { fetchContacts, addContact, deleteContact } from './contactsOps';
 
+import { selectValueFilter } from './filtersSlice';
+
 const handlePending = state => {
-  state.contacts.loading = true;
+  state.loading = true;
 };
 
 const handleRejected = (state, action) => {
-  state.contacts.loading = false;
-  state.contacts.error = action.payload;
+  state.loading = false;
+  state.error = action.payload;
 };
 
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
-    contacts: {
-      items: [],
-      loading: false,
-      error: null,
-    },
-    filters: {
-      name: '',
-    },
-  },
-
-  reducers: {
-    setFilterContact(state, action) {
-      state.filters.name = action.payload;
-    },
+    items: [],
+    loading: false,
+    error: null,
   },
 
   extraReducers: builder => {
     builder
       .addCase(fetchContacts.pending, handlePending)
       .addCase(fetchContacts.fulfilled, (state, action) => {
-        state.contacts.loading = false;
-        state.contacts.error = null;
-        state.contacts.items = action.payload;
+        state.loading = false;
+        state.error = null;
+        state.items = action.payload;
       })
       .addCase(fetchContacts.rejected, handleRejected)
 
       .addCase(addContact.pending, handlePending)
       .addCase(addContact.fulfilled, (state, action) => {
-        state.contacts.loading = false;
-        state.contacts.error = null;
-        state.contacts.items.push(action.payload);
+        state.loading = false;
+        state.error = null;
+        state.items.push(action.payload);
       })
       .addCase(addContact.rejected, handleRejected)
 
       .addCase(deleteContact.pending, handlePending)
       .addCase(deleteContact.fulfilled, (state, action) => {
-        state.contacts.loading = false;
-        state.contacts.error = null;
-        const index = state.contacts.items.findIndex(
+        state.loading = false;
+        state.error = null;
+        const index = state.items.findIndex(
           contact => contact.id === action.payload.id
         );
-        state.contacts.items.splice(index, 1);
+        state.items.splice(index, 1);
       })
       .addCase(deleteContact.rejected, handleRejected);
   },
 });
 
-const selectContacts = state => state.contacts.contacts.items;
-const selectValueFilter = state => state.contacts.filters.name;
+const selectContacts = state => state.contacts.items;
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectValueFilter],
   (contacts, valueFilter) => {
@@ -73,9 +63,7 @@ export const selectFilteredContacts = createSelector(
   }
 );
 
-export const selectLoading = state => state.contacts.contacts.loading;
-export const selectError = state => state.contacts.contacts.error;
-
-export const { setFilterContact } = contactsSlice.actions;
+export const selectLoading = state => state.contacts.loading;
+export const selectError = state => state.contacts.error;
 
 export default contactsSlice.reducer;
